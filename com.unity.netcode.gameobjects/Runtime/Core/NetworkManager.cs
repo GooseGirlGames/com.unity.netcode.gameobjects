23931244de7fa4661b5f84ba5fc9f7b2d08ee073
Noel Stephens <noel.stephens@unity3d.com>
2022-07-22 10:12:05 -0500

    fix: NetworkAnimator does not unsubscribe to OnClientConnectedCallback [MTT-4080] (#2074)

--- com.unity.netcode.gameobjects/Runtime/Core/NetworkManager.cs
+++ com.unity.netcode.gameobjects/Runtime/Core/NetworkManager.cs
@@ -1391,6 +1391,19 @@ namespace Unity.Netcode
             }
 
             IsConnectedClient = false;
+
+            // We need to clean up NetworkObjects before we reset the IsServer
+            // and IsClient properties. This provides consistency of these two
+            // property values for NetworkObjects that are still spawned when
+            // the shutdown cycle begins.
+            if (SpawnManager != null)
+            {
+                SpawnManager.DespawnAndDestroyNetworkObjects();
+                SpawnManager.ServerResetShudownStateForSceneObjects();
+
+                SpawnManager = null;
+            }
+
             IsServer = false;
             IsClient = false;
 
@@ -1411,14 +1424,6 @@ namespace Unity.Netcode
             if (NetworkConfig?.NetworkTransport != null)
             {
                 NetworkConfig.NetworkTransport.OnTransportEvent -= HandleRawTransportPoll;
-            }
-
-            if (SpawnManager != null)
-            {
-                SpawnManager.DespawnAndDestroyNetworkObjects();
-                SpawnManager.ServerResetShudownStateForSceneObjects();
-
-                SpawnManager = null;
             }
 
             if (DeferredMessageManager != null)
